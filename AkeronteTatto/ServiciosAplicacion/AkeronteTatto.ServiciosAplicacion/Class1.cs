36a685497a893c379bcc45fed06246f06f27edcc
Jddog <josedaniel9_5@hotmail.com>
2020-09-05 15:11:34 -0500

    Estructura inicial de back end

---
+++ AkeronteTatto/ServiciosAplicacion/AkeronteTatto.ServiciosAplicacion/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkeronteTatto.ServiciosAplicacion
+{
+    public class Class1
+    {
+    }
+}
